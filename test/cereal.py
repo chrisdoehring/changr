473218c4e8a6a2ac0646c022940c0a15b7b54ed9
chris doehring <chrisdoehring@gmail.com>
2014-08-03 00:01:55 -0700

    Adding ui.

---
+++ test/cereal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+import json,httplib
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
+connection.request('POST', '/1/functions/cereal', json.dumps({
+       "nodeId": "UoLINPVEzv"
+     }), {
+       "X-Parse-Application-Id": "uHud4AZfcs17eXowYDY2eOmQiH8a9EPJv4NDwhMo",
+       "X-Parse-REST-API-Key": "w4EGf0JWDF8qZFxgsGc8hgAYYlestB5DcNEY244h",
+       "Content-Type": "application/json"
+     })
+result = json.loads(connection.getresponse().read())
+print json.dumps(result, indent=2)
+
+
